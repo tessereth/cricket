ec49cd8cc893ae274c4bbe880e925a7e5b3b9178
tessereth <tessereth@yahoo.com.au>
2024-10-02 20:46:02 +1000

    Add filters and team flags

--- replay/fixtures.js
+++ replay/fixtures.js
@@ -1,8 +1,25 @@
 const fixturesUri = 'https://apiv2.cricket.com.au/web/views/fixtures?jsconfig=eccn%3Atrue&format=json'
 
-async function getFixtures(params) {
+const filters = {
+  ausMen: { teamId: 23 },
+  ausWomen: { teamId: 68 },
+  sheffieldShield: { competitionId: 2688 },
+  marshCup: { competitionId: 2686 },
+  WNCL: { competitionId: 2687 },
+  BBL: { competitionId: 2673 },
+  WBBL: { competitionId: 2667 },
+}
+
+async function getFixtures() {
   const uri = URL.parse(fixturesUri)
   uri.searchParams.set('CompletedFixturesCount', 10)
+  const filter = new URLSearchParams(window.location.search).get('filter')
+  if (filter && filters.hasOwnProperty(filter)) {
+    params = filters[filter]
+    for (const [key, value] of Object.entries(params)) {
+      uri.searchParams.set(key, value)
+    }
+  }
 
   const response = await fetch(uri.href)
   const data = await response.json()
@@ -37,10 +54,13 @@ function createCard(fixture) {
   const template = document.getElementById("fixture-template").content
   const card = document.importNode(template, true)
   card.querySelectorAll("[data-replay-link]")[0].href = `replay.html?fixture=${fixture.id}`
-  card.querySelectorAll("[data-image]")[0].src = fixture.competition.imageUrl
+  card.querySelectorAll("[data-competition-image]")[0].src = fixture.competition.imageUrl
   card.querySelectorAll("[data-home-team]")[0].textContent = fixture.homeTeam.name
+  card.querySelectorAll("[data-home-team-image]")[0].src = fixture.homeTeam.logoUrl
   card.querySelectorAll("[data-away-team]")[0].textContent = fixture.awayTeam.name
+  card.querySelectorAll("[data-away-team-image]")[0].src = fixture.awayTeam.logoUrl
   card.querySelectorAll("[data-date]")[0].textContent = formatDate(fixture.startDateTime, fixture.endDateTime)
+  card.querySelectorAll("[data-game-type]")[0].textContent = fixture.gameType
   const footer = [fixture.competition.name, fixture.name, `${fixture.venue.name}, ${fixture.venue.city}`].filter((x) => x).join(" · ")
   card.querySelectorAll("[data-footer]")[0].textContent = footer
   // TODO: update content
